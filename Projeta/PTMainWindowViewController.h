e669ed9baeced0532ad8886e1cf2d639360b1a6f
Michael Wermeester <michael.wermeester@me.com>
2011-08-21 23:38:06 +0200

    cosmetic fix.

--- Projeta/PTMainWindowViewController.h
+++ Projeta/PTMainWindowViewController.h
@@ -8,6 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 #import "PXSourceList.h"
+
 @class MainWindowController;
 
 @interface PTMainWindowViewController : NSViewController <PXSourceListDataSource, PXSourceListDelegate> {
