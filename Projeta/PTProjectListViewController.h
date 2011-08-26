97743ad438875fff53b7f84f1bf41f168e1b53b7
Michael Wermeester <michael.wermeester@me.com>
2011-08-26 12:12:04 +0200

    create ProjectListView.xib and its view controller.

---
+++ Projeta/PTProjectListViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTProjectListViewController.h
+//  Projeta
+//
+//  Created by Michael Wermeester on 26/08/11.
+//  Copyright (c) 2011 Michael Wermeester. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PTProjectListViewController : NSViewController
+
+@end
---
+++ Projeta/PTProjectListViewController.m
@@ -0,0 +1,23 @@
+//
+//  PTProjectListViewController.m
+//  Projeta
+//
+//  Created by Michael Wermeester on 26/08/11.
+//  Copyright (c) 2011 Michael Wermeester. All rights reserved.
+//
+
+#import "PTProjectListViewController.h"
+
+@implementation PTProjectListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
