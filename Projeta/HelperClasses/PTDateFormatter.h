598f57d767a9f667bc4ba49acd83877fc64e8f64
Michael Wermeester <michael.wermeester@me.com>
2012-04-01 00:44:33 +0200

    Subclass NSDateFormatter to allow nil dates.

---
+++ Projeta/HelperClasses/PTDateFormatter.h
@@ -0,0 +1,13 @@
+//
+//  PTDateFormatter.h
+//  Projeta
+//
+//  Created by Michael Wermeester on 4/1/12.
+//  Copyright (c) 2012 Michael Wermeester. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PTDateFormatter : NSDateFormatter
+
+@end
---
+++ Projeta/HelperClasses/PTDateFormatter.m
@@ -0,0 +1,34 @@
+//
+//  PTDateFormatter.m
+//  Projeta
+//
+//  Created by Michael Wermeester on 4/1/12.
+//  Copyright (c) 2012 Michael Wermeester. All rights reserved.
+//
+
+#import "PTDateFormatter.h"
+
+@implementation PTDateFormatter
+
+- (NSDate *)dateFromString:(NSString *)string {
+ 
+    if ((string == nil) || [[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] isEqualTo:@""]) {
+        return nil;
+    } else {
+        return [super dateFromString:string];
+    }
+}
+
+- (BOOL)getObjectValue:(out id *)obj forString:(NSString *)string range:(inout NSRange *)rangep error:(out NSError **)error {
+    
+    if ((string == nil) || [[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] isEqualTo:@""]) {
+        
+        obj = nil;
+        
+        return YES;
+    } else {
+        return [super getObjectValue:obj forString:string range:rangep error:error];
+    }
+}
+
+@end
