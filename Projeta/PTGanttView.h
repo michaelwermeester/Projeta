b0fd66622bd10508bc390839070b3d6762964d4a
Michael Wermeester <michael.wermeester@me.com>
2012-05-05 12:11:58 +0200

    Add PTGanttView class.

---
+++ Projeta/PTGanttView.h
@@ -0,0 +1,13 @@
+//
+//  PTGanttView.h
+//  Projeta
+//
+//  Created by Michael Wermeester on 5/5/12.
+//  Copyright (c) 2012 Michael Wermeester. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PTGanttView : NSView
+
+@end
---
+++ Projeta/PTGanttView.m
@@ -0,0 +1,28 @@
+//
+//  PTGanttView.m
+//  Projeta
+//
+//  Created by Michael Wermeester on 5/5/12.
+//  Copyright (c) 2012 Michael Wermeester. All rights reserved.
+//
+
+#import "PTGanttView.h"
+
+@implementation PTGanttView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
